6fb28d9999659aca91088088278a5f6af7b9c3b6
Emiliano_Fer_mil <emilianofernandezmilki@gmail.com>
2025-03-30 23:24:35 -0300

    ejercicio 1 completo

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ console.log('---------------Practica 1-------------')
 console.log('-----------leer Archivo Como String----------')
 import fs, { readFileSync } from 'fs'
 
-let datos= fs.readFileSync('../Practica1/src/primerDocumentoDePractica.txt', 'utf-8')
+let datos= fs.readFileSync('C:/Users/emife/Repos PNT2/PracticaConMartin/primerDocumentoDePractica.txt', 'utf-8')
 console.log(datos)
 
 // 2.-escribirTextoEnArchivo
 console.log('--------escribir Texto En Archivo----------')
 
 let flag=false
-let ruta ="../Practica1/src/datos2.txt"
+let ruta ="C:/Users/emife/Repos PNT2/PracticaConMartin/Datos2.txt"
 let texto ="prueba de escritura en archivo de texto"
 
 const escribirTexto = (ruta, texto, flag) => {
@@ -28,11 +28,11 @@ const escribirTexto = (ruta, texto, flag) => {
     }
 }
 
-let datos2= escribirTexto(ruta, texto, flag)
+escribirTexto(ruta, texto, flag)
 flag = true
 escribirTexto(ruta, texto, flag)
 //pisarContenido
-datos2= fs.writeFileSync(ruta, 'a partir de ahora')
+let datos2= fs.writeFileSync('C:/Users/emife/Repos PNT2/PracticaConMartin/Datos2.txt', 'a partir de ahora')
 datos2= fs.readFileSync(ruta, 'utf-8')
 console.log(datos2)
 
@@ -42,18 +42,18 @@ datos2= fs.readFileSync(ruta, 'utf-8')
 console.log(datos2)
 
 
-fs.renameSync(ruta, '../Practica1/datos2.txt')
-datos2= fs.readFileSync('../Practica1/datos2.txt', 'utf-8')
+fs.renameSync(ruta, 'C:/Users/emife/Repos PNT2/PracticaConMartin/subCarpeta/Datos2.txt')
+datos2= fs.readFileSync('C:/Users/emife/Repos PNT2/PracticaConMartin/subCarpeta/Datos2.txt', 'utf-8')
 console.log(datos2)
-fs.unlinkSync('../Practica1/datos2.txt')
-console.log(fs.readdirSync('../Practica1'))
+fs.unlinkSync('C:/Users/emife/Repos PNT2/PracticaConMartin/subCarpeta/Datos2.txt')
+console.log(fs.readdirSync('C:/Users/emife/Repos PNT2'))
 
 //transformarStringEnArrayDeNumeros
 console.log('--------transformar String En Array De Numeros----------')
 let stringDeNumeros ="12, 14, 16, 33, 67, 115"
 console.log(stringDeNumeros)
 
-let arrayDeNumeros= stringDeNumeros.split(',')
+let arrayDeNumeros= stringDeNumeros.split(', ').map(Number)
 console.log(arrayDeNumeros)
 
 //transformarArrayDeNumerosAUnSoloString
@@ -63,25 +63,36 @@ console.log(nuevoStringDeNumeros)
 
 //combinarDosArrays
 console.log('--------combinar Dos Arrays----------')
-let array1=[2,8, 16, 51, 999, 1187]
+let array1=[2, 8, 16, 51, 999, 1187]
 let array2=[2, 6, 16, 33, 41, 92, 1266]
+console.log(typeof(array1))
 
-
-
-let combinado = array1.concat(array2)
+let combinado = [...new Set([...array1,...array2])].sort((a,b) => a-b);
 console.log(combinado)
 
-let unificado = [new Set(combinado)]
+//combinado = array1.concat(array2)
+//console.log(combinado)
+
+let unificado = [...new Set([...combinado])]
+console.log(typeof(unificado))
+//combinado = [...combinado.sort((a,b) => a-b)];
 console.log(unificado)
 //let ordenado = unificado.sort(unificado)
 //console.log(ordenado)
-
-
-
-
-
 
 console.log(unificado)
 
 //let ordenado = unificado.sort((a, b) => a - b);
 //console.log(ordenado)
+
+const combinarNArrays = (a) => {
+    let resultado=[];
+    for (let i=0; i<a.length;i++){
+        for (let j=0;j<a[i].length;j++){
+            resultado.push(a[i][j])
+        }
+    }
+    return resultado
+}
+let arrayDeArrays = [[1,3,10],[2,3,15,16],[4],[6,7,13]]
+console.log(combinarNArrays (arrayDeArrays))
